36cf40da4472a5ca7adf797b57eb7997405da8fa
boyone <boyone.g@gmail.com>
2023-06-23 14:50:45 +0700

    Add shell script

---
+++ shopping-cart/all-without-and.sh
@@ -0,0 +1,3 @@
+newman run shoppping-cart-remote.postman_collection.json -d data/shopping-cart.json \ 
+newman run shoppping-cart-remote.postman_collection.json -d data/shopping-cart2.json \ 
+newman run shoppping-cart-remote.postman_collection.json -d data/shopping-cart3.json
